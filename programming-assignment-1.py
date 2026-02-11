02bd3b8c8623082ce5eaf30eef66a80f45ef0b79
Danie1Winn <daniel.winn.biz@gmail.com>
2026-02-11 13:31:57 -0800

    fix(random_animals): Removed method that generates a random seed using memory ID and instead uses a global seed that is iterated each loop.

--- programming-assignment-1.py
+++ programming-assignment-1.py
@@ -1,3 +1,5 @@
+seed = 12345 # Declare a global "seed" variable for the random number generator
+
 def animal_palindrome(animals):
     """
     Flips the list of animals, checks if the flipped list is the same.
@@ -9,14 +11,14 @@ def random_animals(length):
     """
     Creates a list of random animals using the specified length
     """
+    global seed
+
     animals = ["bear", "chicken", "lion", "tiger", "ostrich"] # "Pool" of animals to pull from.
-    dummy = object() # Creates a dummy object in the user's memory
-    seed = id(dummy) # Checks the memory address of the dummy (to use as a random seed)
 
     random_animal_list = []
     for i in range(length):
         # LCG algorithm (Xn+1 = (a Xn + c) mod m), which generates a pseudo-random number
-        seed = (seed * 1103515245 + 12345) & 0x7fffffff
+        seed = (seed * 1103515245 + 12345) & 0x7fffffff # Updates the seed with the LCG algorithm, so the next call (if looped) will produce another random list
         random_animal_list.append(animals[seed % len(animals)])
 
     return random_animal_list
