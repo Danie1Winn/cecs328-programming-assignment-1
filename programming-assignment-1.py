9a8f6ecf80e1d8383a15ca65211c13e1ee7557dd
Danie1Winn <daniel.winn.biz@gmail.com>
2026-02-11 13:20:29 -0800

    feat(main): Added a loop with menu options to enter animals manually, generate animals, or quit. Catches when user enters an invalid number.

--- programming-assignment-1.py
+++ programming-assignment-1.py
@@ -22,4 +22,22 @@ def random_animals(length):
     return random_animal_list
 
 if __name__ == "__main__":
-    print(random_animals(5))
+    while True:
+        print("Choose an option: ")
+        print("1. Enter a list of animals")
+        print("2. Generate a random list of animals")
+        print("3. Quit")
+        choice = input()
+
+        if choice == "1":
+            user_animals = input("\nEnter animals separated by spaces: ")
+            animal_list = user_animals.split()
+            print(f"Your list of animals: {animal_list}")
+            print(f"Flipped list of animals: {animal_list[::-1]}")
+            print(f"Symmetric?: {animal_palindrome(animal_list)}\n")
+
+        elif choice == "3":
+            break
+
+        else:
+            print("Please enter a valid option (1, 2, or 3).")
